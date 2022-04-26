c45a7c448632f0eb6202adc6fcde9741f8d2aac1
mrdude2478 <mrdude2478@gmail.com>
2022-04-26 01:37:20 +0100

    Add files via upload

---
+++ include/Plutonium/docs/df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.js
@@ -0,0 +1,22 @@
+var structpu_1_1ui_1_1render_1_1_renderer_init_options =
+[
+    [ "RendererInitOptions", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a7848ebce825fe19017add859efe618f4", null ],
+    [ "SetExtraDefaultFontSize", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a32d5cf94d1f6bff9d7b4b90c93cbd504", null ],
+    [ "UseAudio", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#ac2901bc764cc2a5fc8cbeb62de0a772a", null ],
+    [ "UseImage", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a2eb74956016e2eea77f1b40d93dba5cb", null ],
+    [ "UseRomfs", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#acbe8272f338247638d1a603dc00de07f", null ],
+    [ "UseTTF", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a91145726cdb1f2b358baef5073942648", null ],
+    [ "audio_mixer_flags", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a404582159cc98e75c42bb8ad6e113c3f", null ],
+    [ "default_font_path", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#aa064b0fd59c49602b364117809a4800e", null ],
+    [ "extra_default_font_sizes", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a18a9ec1702ef30964391be62f777a1ad", null ],
+    [ "height", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a072f8793d8e3e2e670dc782554abc131", null ],
+    [ "init_img", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a90f6cc91a32d2d12a8d3b1c99b8cf403", null ],
+    [ "init_mixer", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a5795fa97895c9dfc1d2b583c75f19d7f", null ],
+    [ "init_pl", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a7fc319d0d55ffb5f1b73d9a0f8366b3f", null ],
+    [ "init_romfs", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a7aeb5c4f0f793bfd13590668cd43235a", null ],
+    [ "init_ttf", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a20c992b4a0bbafe56a304e01e688852f", null ],
+    [ "sdl_flags", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#afa4665c4893bdfd09b3d039c1e3a7e86", null ],
+    [ "sdl_img_flags", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#ab713bbdc588bcf1cc55255db74701998", null ],
+    [ "sdl_render_flags", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#a3d91fb198cc5867cdb6ae8aadd0281e8", null ],
+    [ "width", "df/d6f/structpu_1_1ui_1_1render_1_1_renderer_init_options.html#aa042751d8759c060e7b845832cc00ef0", null ]
+];
